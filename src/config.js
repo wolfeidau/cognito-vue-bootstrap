804064cc6701eb2a837a342f8273f687a561c36d
Mark Wolfe <mark@wolfe.id.au>
2017-02-09 19:29:51 +1100

    Added dotenv support to provide env vars during build.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  region: '',
-  IdentityPoolId: '',
-  UserPoolId: '',
-  ClientId: ''
+  region: process.env.AWS_REGION,
+  IdentityPoolId: process.env.COGNITO_IDENTITY_POOL_ID,
+  UserPoolId: process.env.COGNITO_USER_POOL_ID,
+  ClientId: process.env.COGNITO_CLIENT_ID
 }
