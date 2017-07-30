464f3a8126f9751eb994b2be9b14807a6b4f8f4e
Mark Wolfe <mark@wolfe.id.au>
2017-07-30 10:35:39 +1000

    chore(Webpack) Updated to the latest webpack template and fixes.

---
+++ src/router/index.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home.vue'
+import Login from '@/components/Login.vue'
+import Signup from '@/components/Signup.vue'
+import Confirm from '@/components/Confirm.vue'
+import Dashboard from '@/components/Dashboard.vue'
+import PasswordChange from '@/components/PasswordChange.vue'
+import PasswordReset from '@/components/PasswordReset.vue'
+import PasswordResetConfirm from '@/components/PasswordResetConfirm.vue'
+import cognitoAuth from '@/cognito'
+Vue.use(Router)
+
+function requireAuth (to, from, next) {
+  cognitoAuth.isAuthenticated((err, loggedIn) => {
+    console.log('requireAuth login check', err, loggedIn)
+    if (err) return next()
+    if (!loggedIn) {
+      next({
+        path: '/login',
+        query: { redirect: to.fullPath }
+      })
+    } else {
+      next()
+    }
+  })
+}
+
+export default new Router({
+  linkActiveClass: 'active',
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/', component: Home },
+    { path: '/dashboard', component: Dashboard, beforeEnter: requireAuth },
+    { path: '/login', component: Login },
+    { path: '/signup', component: Signup },
+    { path: '/confirm', component: Confirm },
+    { path: '/change_password', component: PasswordChange, beforeEnter: requireAuth },
+    { path: '/reset_password', component: PasswordReset },
+    { path: '/confirm_reset_password', component: PasswordResetConfirm },
+    { path: '/logout',
+      beforeEnter (to, from, next) {
+        cognitoAuth.logout()
+        next('/')
+      }
+    }
+  ]
+})
