8a7973f2454b40abc3ea0248f0b8fd82dcd89e27
Mark Wolfe <mark@wolfe.id.au>
2017-07-31 19:31:03 +1000

    fix(UnitTests) Upgrade configuration files for unit tests.

--- test/unit/specs/Home.spec.js
+++ test/unit/specs/Home.spec.js
@@ -1,13 +1,22 @@
 import Vue from 'vue'
-import Home from 'src/components/Home'
+import Home from '@/components/Home'
+
+// var cognitoAuth = {
+//   isAuthenticated: (cb) => {
+//     return cb(null, true)
+//   }
+// }
 
 describe('Home.vue', () => {
   it('should render correct contents', () => {
-    const vm = new Vue({
-      el: document.createElement('div'),
-      render: (h) => h(Home)
-    })
-    expect(vm.$el.querySelector('h2').textContent)
+    // const vm = new Vue({
+    //   el: document.createElement('div'),
+    //   cognitoAuth,
+    //   render: (h) => h(Home)
+    // })
+    const Constructor = Vue.extend(Home)
+    const HomeComponent = new Constructor().$mount()
+    expect(HomeComponent.$el.querySelector('h2').textContent)
       .to.equal('Welcome')
   })
 })
