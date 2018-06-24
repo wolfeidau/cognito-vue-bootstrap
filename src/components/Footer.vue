55e637b75c3b1f71bc158dbf7a466750dc18f777
Mark Wolfe <mark@wolfe.id.au>
2018-06-24 22:40:38 +1000

    feat(Footer) Added a footer and tweaked layout, added links.

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="bd-footer">
+    <b-container>
+      <b-row class="justify-content-md-center">
+        <div class="bd-footer-copyright">© 2018 Company, Inc.</div>
+      </b-row>
+    </b-container>
+  </footer>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.bd-footer {
+  margin-top: 2em;
+  border-top: .2rem #f7f7f7 solid !important;
+}
+
+.bd-footer-copyright {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+</style>
